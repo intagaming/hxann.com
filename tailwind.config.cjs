272ed2a37adce30182d9fb46da2dd80d9e9ebde3
An Xuan Hoang <xuanan2001@gmail.com>
2022-06-24 18:48:38 +0700

    chore: font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,12 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}"],
   darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        serif: ["Lora", "serif"],
+        sans: ["Open Sans", "sans-serif"],
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 };
