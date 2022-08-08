9fa4cdd2a860d52d7be0ea51c0c7c4801b49f9fe
An Hoang <xuanan2001@pm.me>
2022-08-08 21:58:39 +0700

    remove usehooks-ts; fix Utterances client:only

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,6 +1,32 @@
-import { useRef, useState } from "react";
-import { useOnClickOutside } from "usehooks-ts";
+import { RefObject, useEffect, useRef, useState } from "react";
 import ThemeToggle from "./ThemeToggle";
+
+// From https://hashnode.com/post/useonclickoutside-custom-hook-to-detect-the-mouse-click-on-outside-typescript-ckrejmy3h0k5r91s18iu42t28
+type Event = MouseEvent | TouchEvent;
+
+const useOnClickOutside = <T extends HTMLElement = HTMLElement>(
+  ref: RefObject<T>,
+  handler: (event: Event) => void
+) => {
+  useEffect(() => {
+    const listener = (event: Event) => {
+      const el = ref?.current;
+      if (!el || el.contains((event?.target as Node) || null)) {
+        return;
+      }
+
+      handler(event); // Call the handler only if the click is outside of the element passed.
+    };
+
+    document.addEventListener("mousedown", listener);
+    document.addEventListener("touchstart", listener);
+
+    return () => {
+      document.removeEventListener("mousedown", listener);
+      document.removeEventListener("touchstart", listener);
+    };
+  }, [ref, handler]); // Reload only if ref or handler changes
+};
 
 export type NavBarEntry = {
   link: string;
