a7d0ca388526195c3ef831fabb431d08a43d7be3
An Hoang <xuanan2001@pm.me>
2023-04-29 13:16:01 +0700

    fix: Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,13 @@
 const Footer = () => {
   return (
-    <footer className="min-h-32 flex flex-col items-center justify-center border-t py-10 dark:bg-neutral-900 dark:text-white">
-      <p>Made by An Hoang with ❤️</p>
-      <p>
+    <footer className="min-h-32 flex flex-col items-center justify-center border-t px-4 py-10 text-center dark:bg-neutral-900 dark:text-white">
+      <p className="mb-6">Made by An Hoang with ❤️</p>
+
+      <p className="mb-2">
         This <i>personal</i> blog is open source. Check it out.
       </p>
 
-      <div className="my-6 flex">
+      <div className="mb-6 flex">
         <a href="https://github.com/intagaming/hxann.com">
           <i className="fa-brands fa-github fa-xl"></i>
         </a>
