64b6f6140c15741dc9d37f634a1930e9c9840415
An Hoang <xuanan2001@pm.me>
2023-05-02 21:23:21 +0700

    fix: SEO defaults

---
+++ src/seo.ts
@@ -0,0 +1,13 @@
+import type { SEO } from "@/types";
+
+export const defaultSeo: SEO = {
+  title: "An Hoang",
+  description: "software.",
+  openGraph: {
+    title: "An Hoang",
+    type: "website",
+    description: "software.",
+    url: "https://hxann.com",
+    image: `https://hxann.com/images/banner.png`,
+  },
+};
